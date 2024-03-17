acf5adbf24308b74a6ed6ab9fcab5316f2f3b227
Adedoyin Samuel <60210384+SammyGIS@users.noreply.github.com>
2024-03-17 17:40:55 +0100

    Update analytics_query.sql

--- analytics_query.sql
+++ analytics_query.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE TABLE `sammygis.uber_data_engineering_yt.tbl_analytics` AS (
+CREATE OR REPLACE TABLE `uber-analytics-project-413108.uber_data_engineering_yt.tbl_analytics` AS (
 SELECT 
-f.trip_id,
+f.trip_distance_id,
 f.VendorID,
 d.tpep_pickup_datetime,
 d.tpep_dropoff_datetime,
@@ -20,13 +20,12 @@ f.tolls_amount,
 f.improvement_surcharge,
 f.total_amount
 FROM 
-
-`sammygis.uber_data_engineering_yt.fact_table` f
-JOIN `sammygis.uber_data_engineering_yt.datetime_dim` d  ON f.datetime_id=d.datetime_id
-JOIN `sammygis.uber_data_engineering_yt.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
-JOIN `sammygis.uber_data_engineering_yt.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
-JOIN `sammygis.uber_data_engineering_yt.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
-JOIN `sammygis.uber_data_engineering_yt.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
-JOIN `sammygis.uber_data_engineering_yt.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
-JOIN `sammygis.uber_data_engineering_yt.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
+`uber-analytics-project-413108.uber_data_engineering_yt.fact_table` f
+JOIN `uber-analytics-project-413108.uber_data_engineering_yt.datetime_dim` d  ON f.datetime_id=d.datetime_id
+JOIN `uber-analytics-project-413108.uber_data_engineering_yt.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
+JOIN `uber-analytics-project-413108.uber_data_engineering_yt.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
+JOIN `uber-analytics-project-413108.uber_data_engineering_yt.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
+JOIN `uber-analytics-project-413108.uber_data_engineering_yt.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
+JOIN `uber-analytics-project-413108.uber_data_engineering_yt.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
+JOIN `uber-analytics-project-413108.uber_data_engineering_yt.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
 ;
