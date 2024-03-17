479be2c336db71119ade46786ab3c0931509109d
Adedoyin Samuel <60210384+SammyGIS@users.noreply.github.com>
2024-03-17 17:41:57 +0100

    Update extract.py

--- mage-files/extract.py
+++ mage-files/extract.py
@@ -1,26 +1,30 @@
-import io
-import pandas as pd
-import requests
-if 'data_loader' not in globals():
-    from mage_ai.data_preparation.decorators import data_loader
-if 'test' not in globals():
-    from mage_ai.data_preparation.decorators import test
+from mage_ai.data_preparation.repo_manager import get_repo_path
+from mage_ai.io.bigquery import BigQuery
+from mage_ai.io.config import ConfigFileLoader
+from pandas import DataFrame
+from os import path
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
 
 
-@data_loader
-def load_data_from_api(*args, **kwargs):
+@data_exporter
+def export_data_to_big_query(data, **kwargs) -> None:
     """
-    Template for loading data from API
-    """
-    url = 'https://storage.googleapis.com/uber-data-engineering-project-darshil/uber_data.csv'
-    response = requests.get(url)
+    Template for exporting data to a BigQuery warehouse.
+    Specify your configuration settings in 'io_config.yaml'.
 
-    return pd.read_csv(io.StringIO(response.text), sep=',')
+    Docs: https://docs.mage.ai/design/data-loading#bigquery
 
+    
+    """
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
 
-@test
-def test_output(output, *args) -> None:
-    """
-    Template code for testing the output of the block.
-    """
-    assert output is not None, 'The output is undefined'
+    for key, value in data.items():
+        table_id = 'uber-analytics-project-413108.uber_data_engineering_yt.{}'.format(key)
+        BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
+            DataFrame(value),
+            table_id,
+            if_exists='replace',  # Specify resolution policy if table name already exists
+        )
